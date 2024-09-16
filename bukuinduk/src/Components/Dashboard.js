633814a4d5ace1c17f4390c47761b54fbf668b30
Aiko <jokoaiko8b@gmail.com>
2024-09-16 08:56:42 +0700

    mengedit tampilan checkbox login box

--- bukuinduk/src/Components/Dashboard.js
+++ bukuinduk/src/Components/Dashboard.js
@@ -24,7 +24,7 @@ const Dashboard = () => {
     return (
         <div>
             <div className="group">
-                <div className='w-[284px] h-full rounded-[0_16px_16px_0] border border-white '>
+                <div className='w-[284px] h-full rounded-[0_16px_16px_0] border border-gray-100'>
                     <header
                         className='w-full h-[103px] p-[24px] flex items-center justify-normal gap-[10px] rounded-[0_16px_0_0]'
                         style={{ backgroundColor: 'rgba(87, 117, 186, 0.8)' }}>
@@ -82,7 +82,7 @@ const Dashboard = () => {
                         </div>
                     </nav>
 
-                    <hr className='w-[284px] border border-gray-100' />
+                    <hr className='w-[280px] border border-gray-100' />
                     <div className='w-fill h-[468px] p-[24px] gap-2'>
                         <div className='w-[236px] h-[40px] rounded-[8px] p-[10px_12px_10px_12px] gap-[10px] flex items-center hover:bg-gray-100'>
                             <IoMdExit className='w-[20px] h-[20px]' /><button onClick={() => navigate('/')} className='font-PTSans font-bold text-[15px] text-left leading-5'>Keluar</button>
--- bukuinduk/src/Components/LoginBox.js
+++ bukuinduk/src/Components/LoginBox.js
@@ -1,68 +1,98 @@
 import { useNavigate } from "react-router-dom";
 import { LiaAngleDownSolid } from "react-icons/lia";
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
 const LoginBox = () => {
-    const navigate = useNavigate()
-    
-    const [selectedValue, setSelectedValue] = useState('Angkatan')
-    const [isOpen, setIsOpen] = useState(false)
+  const navigate = useNavigate();
+  const [selectedValue, setSelectedValue] = useState('Angkatan');
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleCheckboxChange = (value) => {
-        setSelectedValue(value)
-        setIsOpen(false)
-    }
+  const handleCheckboxChange = (value) => {
+    setSelectedValue(value);
+    setIsOpen(false);
+  };
 
-    return (
-        <div className="w-full h-full flex justify-center items-center">
-            <div className="border border-slate-300 rounded-[10px] px-6 py-10 flex flex-col justify-center items-center shadow-custom">
-                <h1 className="text-2xl font-PTSans font-bold mb-4">SELAMAT DATANG</h1>
-                <div className="relative w-[476px] h-[70px] mb-6 border border-gray-300 rounded-[10px] font-PTSans">
-                    <input type="email" placeholder="Email" className="w-full h-full rounded-[10px] placeholder-black py-4 px-6" id="input" />
-                </div>
-                <div className="relative w-[476px] h-[70px] mb-6 border border-gray-300 rounded-[10px] font-PTSans">
-                    <input type="password" placeholder="Password" className="w-full h-full rounded-[10px] placeholder-black py-4 px-6" id="input" />
-                </div>
-                <div className="relative w-[476px] h-[70px] mb-6 border border-gray-300 rounded-[10px] font-PTSans flex items-center py-4 px-6">
-                    <div
-                        className="flex items-center cursor-pointer"
-                        onClick={() => setIsOpen(!isOpen)}
-                    >
-                        <LiaAngleDownSolid />
-                        <label className="font-PTSans ml-6">{selectedValue}</label>
-                    </div>
-                    {isOpen && (
-                        <ul className="absolute top-full mt-2 left-0 w-full h-auto bg-white border border-gray-300 rounded-[10px] px-4 py-4">
-                            <li>
-                                <input
-                                    type="checkbox"
-                                    name="value1"
-                                    onChange={() => handleCheckboxChange('2020')}
-                                />
-                                2020
-                            </li>
-                            <li>
-                                <input
-                                    type="checkbox"
-                                    name="value2"
-                                    onChange={() => handleCheckboxChange('2021')}
-                                />
-                                2021
-                            </li>
-                            <li>
-                                <input
-                                    type="checkbox"
-                                    name="value3"
-                                    onChange={() => handleCheckboxChange('2022')}
-                                />
-                                2022
-                            </li>
-                        </ul>
-                    )}
-                </div>
-                <button onClick={() => navigate('/admindashboard')} type="button" className="w-[476px] h-[70px] bg-button text-2xl text-white font-PTSans rounded-[10px] p-[10px]">MASUK</button>
-            </div>
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="border border-slate-300 rounded-[10px] px-6 py-10 flex flex-col justify-center items-center shadow-custom">
+        <h1 className="text-2xl font-PTSans font-bold mb-4">SELAMAT DATANG</h1>
+        <div className="relative w-[476px] h-[70px] mb-6 border border-gray-300 rounded-[10px] font-PTSans">
+          <input type="email" placeholder="Email" className="w-full h-full rounded-[10px] placeholder-black py-4 px-6" id="input" />
         </div>
-    );
+        <div className="relative w-[476px] h-[70px] mb-6 border border-gray-300 rounded-[10px] font-PTSans">
+          <input type="password" placeholder="Password" className="w-full h-full rounded-[10px] placeholder-black py-4 px-6" id="input" />
+        </div>
+        <div className="relative w-[476px] h-[70px] mb-6 border border-gray-300 rounded-[10px] font-PTSans flex items-center py-4 px-6">
+          <div
+            className="flex items-center cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <LiaAngleDownSolid />
+            <label className="font-PTSans ml-6">{selectedValue}</label>
+          </div>
+          {isOpen && (
+            <ul className="absolute top-full mt-2 left-0 w-full h-auto bg-white border border-gray-300 rounded-[10px] px-4 py-4">
+              <li>
+                <label className="flex items-center cursor-pointer">
+                  <span
+                    className={`inline-block w-4 h-4 rounded-full mr-2 ${
+                      selectedValue === '2020' ? 'bg-blue-500' : 'bg-gray-300'
+                    }`}
+                  ></span>
+                  2020
+                  <input
+                    type="checkbox"
+                    className="hidden"
+                    onChange={() => handleCheckboxChange('2020')}
+                    checked={selectedValue === '2020'}
+                  />
+                </label>
+              </li>
+              <li>
+                <label className="flex items-center cursor-pointer">
+                  <span
+                    className={`inline-block w-4 h-4 rounded-full mr-2 ${
+                      selectedValue === '2021' ? 'bg-blue-500' : 'bg-gray-300'
+                    }`}
+                  ></span>
+                  2021
+                  <input
+                    type="checkbox"
+                    className="hidden"
+                    onChange={() => handleCheckboxChange('2021')}
+                    checked={selectedValue === '2021'}
+                  />
+                </label>
+              </li>
+              <li>
+                <label className="flex items-center cursor-pointer">
+                  <span
+                    className={`inline-block w-4 h-4 rounded-full mr-2 ${
+                      selectedValue === '2022' ? 'bg-blue-500' : 'bg-gray-300'
+                    }`}
+                  ></span>
+                  2022
+                  <input
+                    type="checkbox"
+                    className="hidden"
+                    onChange={() => handleCheckboxChange('2022')}
+                    checked={selectedValue === '2022'}
+                  />
+                </label>
+              </li>
+            </ul>
+          )}
+        </div>
+        <button
+          onClick={() => navigate('/admindashboard')}
+          type="button"
+          className="w-[476px] h-[70px] bg-button text-2xl text-white font-PTSans rounded-[10px] p-[10px]"
+        >
+          MASUK
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginBox;
